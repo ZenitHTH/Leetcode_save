39e3768240569b5bf6edfd03c02cbc0f772442be
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-08-07 15:33:27 +0700

    fixed out of range

--- Median of Two Sorted Arrays.py
+++ Median of Two Sorted Arrays.py
@@ -4,53 +4,61 @@ def floop (lnum1:list[int],lnum2:list[int]):
     j=0
     while True:
 
-        if i < len(lnum1)-1 or j < len(lnum2)-1 :
-            x,y=-2,-2
-            try :
-                x = lnum1[i]
-            except :
-                print(f"lnum1[{i}] is out of range")
-            try : 
-                y = lnum2[j]
-            except :
-                print(f"lnum2[{j}] is out of range")
-            print(f"pick lnum1:{x} ,lnum2:{y}")
-        if (lnum1[i] > lnum2[j] ) or lnum1[i] == lnum2[j]:
-            print(f"in case 1")
-            lnum1.insert(i,lnum2[j])
-            #if j < len(lnum2)-1:
-            j+=1
-            #i+=1
-        elif ( lnum1[i] < lnum2[j] ):
-            #print(f"in case 2")
-            if (i < len(lnum1)-1 ) :
-                if (lnum1[i+1] > lnum2[j]):
-                    print(f"in case 2.1")
-                    lnum1.insert(i+1,lnum2[j])
+        if j < len(lnum2) :    
+            print("")
+            print(f"pick lnum1:{lnum1[i]} ,lnum2:{lnum2[j]}")
+            if (lnum1[i] > lnum2[j] ) or lnum1[i] == lnum2[j]:
+                print(f"in case 1")
+                lnum1.insert(i,lnum2[j])
+                if j < len(lnum2):
                     j+=1
-                    #i+=1
-
-            else : #i == len(lnum1)
-                print(f"in case 2.2")
-                lnum1.append(lnum2[j])
-                j+=1
                 #i+=1
-            #if j < len(lnum2)-1:
+            elif ( lnum1[i] < lnum2[j] ):
+                #print(f"in case 2")
+                if (i < len(lnum1)-1 ) :
+                    if (lnum1[i+1] > lnum2[j]):
+                        print(f"in case 2.1")
+                        lnum1.insert(i+1,lnum2[j])
+                        if j < len(lnum2):
+                            j+=1
+                        
+
+                else : #i == len(lnum1)
+                    print(f"in case 2.2")
+                    lnum1.append(lnum2[j])
+                    if j < len(lnum2):
+                        j+=1
+        else :
+            print(f"j is out of range")            
+                
 
         
         print(f"{i},{j}:{lnum1}")
-        i+=1
-        print(f"{i} >= {len(lnum1)-1} and {j} > {len(lnum2)-1} : {(i >= len(lnum1)-1)} and {(j > len(lnum2)-1)}")
+
+
+        print(f"{i+1} >= {len(lnum1)} and {j+1} >= {len(lnum2)} : {(i+1 >= len(lnum1))} and {(j+1 >= len(lnum2))}")
         print("--------------------------------------------------")
         # fix break this here
-        if (i >= len(lnum1)-1) and (j >= len(lnum2)-1) :
+
+        if (i+1 >= len(lnum1)) and (j+1 >= len(lnum2)) :
             break
+
+        if i+1 < len(lnum1):
+            i+=1
+        else :
+            print(f"lnum1 is out of range")
+
  
 def median(lnum:list[int])->float:
     if len(lnum) %2 ==0:
-        return (lnum[int(len(lnum)/2)-1] + lnum[int(len(lnum)/2)])/2
+        i = int(len(lnum)/2)-1
+        j = int(len(lnum)/2)
+        print(f"i,j:{i},{j} -> {lnum[i]}+{lnum[j]}/2 -> {(lnum[i]+lnum[j])/2}")
+        return (lnum[i] + lnum[j])/2
     else :
-        return lnum[int(len(lnum)/2)]
+        i = int(len(lnum)/2)
+        print(f"i:{i} - > {lnum[i]}")
+        return lnum[i]
         
 def run (lnum1:list[int],lnum2:list[int]) :
     if len(lnum1) > len(lnum2):
@@ -64,6 +72,7 @@ def run (lnum1:list[int],lnum2:list[int]) :
         floop(lnum1,lnum2)
         lnum1.remove(-1)
         print(lnum1)
+    print("**********************************************")
     return median(lnum1)
 
 
@@ -78,6 +87,10 @@ class UnitTest(unittest.TestCase):
 
     def test3(self):
         self.assertEqual(run([1,3,5,7,11],[1,2,3,4]),median([1,1,2,3,4,5,7,11]))
+
+    def test4(self):
+        self.assertEqual(run([3],[1,2,4]),median([1,2,3,4]))
+
 if __name__ == "__main__" :      
     unittest.main()
     
