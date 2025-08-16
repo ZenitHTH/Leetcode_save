e4fcc915e1f1b1abc65aa90e1f03312d6f6705c4
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-08-16 23:37:59 +0700

    move pos variable into function

--- Longest_Palindromic_Substring/longest_palindrome/src/lib.rs
+++ Longest_Palindromic_Substring/longest_palindrome/src/lib.rs
@@ -2,10 +2,9 @@ pub struct Solution;
 
 impl Solution {
 
-    pub fn palindrome_checker(s:String) -> String {
+    pub fn palindrome_checker(s:String,pos:usize) -> String {
         let limit_left = 0;
         let limit_right = s.len();
-        let pos = s.len()/2;
         let mut pos_left = pos - 1;
         let mut pos_right = pos + 1;
         loop {
@@ -14,19 +13,19 @@ impl Solution {
                     pos_left -= 1;
                     pos_right += 1;
                 }else {
-                    return s.chars().skip(pos_left).take(pos_right-pos_left+1).collect();
+                    return s.chars().skip(pos_left).take((pos_right-pos_left)+1).collect();
                 }
 
             }else if s.chars().nth(pos_left) == s.chars().nth(pos) {
-                return s.chars().skip(pos_left).take(pos-pos_left+1).collect();
+                return s.chars().skip(pos_left).take((pos-pos_left)+1).collect();
             }
             else if s.chars().nth(pos_right) == s.chars().nth(pos) {
-                return s.chars().skip(pos).take(pos-pos_right+1).collect();
+                return s.chars().skip(pos).take((pos_right-pos)+1).collect();
             }
             else {
                 pos_left += 1;
                 pos_right -= 1;
-                return s.chars().skip(pos_left).take(pos_right-pos_left+1).collect();
+                return s.chars().skip(pos_left).take((pos_right-pos_left)+1).collect();
             }
         }
     }
