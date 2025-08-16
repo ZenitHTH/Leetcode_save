7397ecf48b9541b78c35c1c570b53ec0076da026
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-08-16 23:38:17 +0700

    add more test case for palindrom checker

--- Longest_Palindromic_Substring/longest_palindrome/tests/palindrome_checker_test.rs
+++ Longest_Palindromic_Substring/longest_palindrome/tests/palindrome_checker_test.rs
@@ -4,27 +4,52 @@ mod tests {
     
     #[test]
     fn palindrome_checker_test1() {
-        let result = Solution::palindrome_checker(String::from("bcdcb"));
+        let s:String = String::from("bcdcb");
+        let pos:usize = s.len()/2;
+        let result = Solution::palindrome_checker(s,pos);
         assert_eq!(result,String::from("bcdcb"));
     }
 
     #[test]
     fn palindrome_checker_test2(){
-        let result = Solution::palindrome_checker(String::from("abcdcfg"));
+        let s:String = String::from("abcdcfg");
+        let pos:usize = s.len()/2;
+        let result = Solution::palindrome_checker(s,pos);
         assert_eq!(result,String::from("cdc"));
     }
 
     #[test]
     fn palindrome_checker_test3(){
-        let result:String = Solution::palindrome_checker(String::from("abcddef"));
+        let s:String = String::from("abcddef");
+        let pos:usize = s.len()/2;
+        let result:String = Solution::palindrome_checker(s,pos);
         assert_eq!(result,String::from("dd"))
     }
 
     #[test]
     fn palindrome_checker_test4(){
-        let result:String = Solution::palindrome_checker(String::from("abccdef"));
+        let s:String = String::from("abccdef");
+        let pos:usize = s.len()/2;
+        let result:String = Solution::palindrome_checker(s,pos);
         assert_eq!(result,String::from("cc"));
     }
+
+    #[test]
+    fn palindrome_checker_test5(){
+        let s:String = String::from("abcdef");
+        let pos:usize = s.len()/2;
+        let result:String = Solution::palindrome_checker(s, pos);
+        assert_eq!(result.len(),1);
+    }
+
+    #[test]
+    fn palindrome_checker_test6(){
+        let s:String = String::from("abcdcf");
+        let pos:usize = s.len()/2;
+        let result:String = Solution::palindrome_checker(s, pos);
+        assert_eq!(result,String::from("cdc"));
+    }
+
 
 
 }
