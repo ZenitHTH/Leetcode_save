acc914d516a406e167bb369da0fc483e825cab7b
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2025-08-19 00:56:06 +0700

    fixed bug limit right , move first case into bottom case

--- Longest_Palindromic_Substring/longest_palindrome/src/lib.rs
+++ Longest_Palindromic_Substring/longest_palindrome/src/lib.rs
@@ -4,30 +4,26 @@ impl Solution {
 
     pub fn palindrome_checker(s:String,pos:usize) -> String {
         let limit_left = 0;
-        let limit_right = s.len();
+        let limit_right = s.len()-1;
         let mut pos_left = pos - 1;
         let mut pos_right = pos + 1;
         loop {
-            if s.chars().nth(pos_left) == s.chars().nth(pos_right) {
+            if s.chars().nth(pos_left) == s.chars().nth(pos) && s.chars().nth(pos) != s.chars().nth(pos_right){
+                //do it in case palindrome is even
+                return Solution::palindrome_checker_even(pos_left, pos, s);
+                //return s.chars().skip(pos_left).take((pos-pos_left)+1).collect();
+            } else if s.chars().nth(pos_right) == s.chars().nth(pos) && s.chars().nth(pos_left) != s.chars().nth(pos) {
+                //do it in case palindrome is even
+                return Solution::palindrome_checker_even(pos, pos_right, s);
+                //return s.chars().skip(pos).take((pos_right-pos)+1).collect();
+            } else if s.chars().nth(pos_left) == s.chars().nth(pos_right) {
                 if pos_left > limit_left && pos_right < limit_right {
-                    if pos_left > limit_left {
                         pos_left -= 1;
-                    }
-                    if pos_right < limit_right {
                         pos_right += 1;
-                    }
                     
                 }else {
                     return s.chars().skip(pos_left).take((pos_right-pos_left)+1).collect();
                 }
-
-            }else if s.chars().nth(pos_left) == s.chars().nth(pos) {
-                //do it in case palindrome is even
-                return s.chars().skip(pos_left).take((pos-pos_left)+1).collect();
-            }
-            else if s.chars().nth(pos_right) == s.chars().nth(pos) {
-                //do it in case palindrome is even
-                return s.chars().skip(pos).take((pos_right-pos)+1).collect();
             }
             else {
                 pos_left += 1;
@@ -37,6 +33,48 @@ impl Solution {
         }
     }
 
+    fn vec_get(pos_left:usize,pos_right:usize,v_str:Vec<char>) -> String {
+        match v_str.get(pos_left..(pos_right+1)){
+            Some(v) => {
+                return v.iter().copied().collect::<String>();
+
+            }
+            None => {
+                panic!("cannot get pos_left , pos_right");
+            }
+        }
+    }
+
+    fn palindrome_checker_even(left:usize,right:usize,s:String)->String {
+        let mut pos_left: usize = left;
+        let mut pos_right: usize = right;
+        let limit_left:usize = 0;
+        let limit_right:usize= s.len();
+        let v_str:Vec<char> = s.chars().collect();
+        loop {
+            if v_str.get(pos_left) == v_str.get(pos_right) {
+                if pos_left > limit_left && pos_right < limit_right {
+                    pos_left -= 1;
+                    pos_right +=1;
+                }else{
+                    return Solution::vec_get(pos_left,pos_right,v_str);
+                }
+            }else{
+                pos_left +=  1;
+                pos_right  -=1;
+                return Solution::vec_get(pos_left,pos_right,v_str);
+                // match v_str.get(pos_left..(pos_right+1)){
+                //     Some(v) => {
+                //         return v.iter().copied().collect::<String>();
+
+                //     }
+                //     None => {
+                //         panic!("cannot get pos_left , pos_right");
+                //     }
+                // }
+            }
+        }
+    }
 
     pub fn longest_palindrome(s: String) -> String {
 
