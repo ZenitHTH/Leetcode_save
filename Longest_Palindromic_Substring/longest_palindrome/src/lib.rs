0c9d5098935094eeec9606aedbc500e680a7a19d
Settawat Boriruklert <settawatboriruklert@gmail.com>
2025-08-19 19:20:45 +0700

    move even case go out of loop

--- Longest_Palindromic_Substring/longest_palindrome/src/lib.rs
+++ Longest_Palindromic_Substring/longest_palindrome/src/lib.rs
@@ -8,13 +8,11 @@ impl Solution {
         let mut pos_left = pos - 1;
         let mut pos_right = pos + 1;
         let v_str:Vec<char> = s.chars().collect();
-        loop {
 
-            if v_str.get(pos_left) == v_str.get(pos_right) && v_str.get(pos_left) == v_str.get(pos) {
+        if v_str.get(pos_left) == v_str.get(pos_right) && v_str.get(pos_left) == v_str.get(pos) {
                 return Solution::palindrome_checker_same(pos_left, pos_right, v_str);
-            }
-
-            if v_str.get(pos_left) == v_str.get(pos) {
+        }
+        if v_str.get(pos_left) == v_str.get(pos) {
                 if pos_left >= limit_left {
                     return Solution::palindrome_checker_even(pos_left, pos, s);
                 }else {
@@ -27,8 +25,11 @@ impl Solution {
                 }else {
                     return Solution::vec_get(pos, pos_right, v_str);
                 }
-                
-            } else if v_str.get(pos_left) == v_str.get(pos_right) {
+            }
+             
+        loop {
+
+            if v_str.get(pos_left) == v_str.get(pos_right) {
                 if pos_left > limit_left && pos_right < limit_right {
                         pos_left -= 1;
                         pos_right += 1;
@@ -114,7 +115,7 @@ impl Solution {
             let mut vec_palindrome:Vec<String> = Vec::new();
             
             if s.len() > 3 {
-                for pos in 1..(s.len()-2) {
+                for pos in 1..(s.len()-1) {
                     let res = self::Solution::palindrome_checker(s.clone(), pos);
                     if res.len() > 1 {
                         vec_palindrome.push(res);
